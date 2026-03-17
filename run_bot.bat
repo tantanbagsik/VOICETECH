b538f65f04e6df922e062fd14b8389cebab855ba
raypanganiban0825 <raypanganiban0825@gmail.com>
2026-03-18 04:37:19 +0800

    Update run script

--- run_bot.bat
+++ run_bot.bat
@@ -4,7 +4,7 @@ echo Voice Appointment Bot - Setup & Run
 echo ========================================
 echo.
 
-echo [1/4] Checking Python...
+echo [1/5] Checking Python...
 python --version
 if errorlevel 1 (
     echo Python not found! Install from https://python.org
@@ -13,16 +13,24 @@ if errorlevel 1 (
 )
 
 echo.
-echo [2/4] Downloading/updating code...
-git clone https://github.com/tantanbagsik/VOICETECH.git
+echo [2/5] Getting code...
+cd %USERPROFILE%\Desktop
+if not exist "VOICETECH" (
+    git clone https://github.com/tantanbagsik/VOICETECH.git
+)
 cd VOICETECH
+git pull
 
 echo.
-echo [3/4] Installing dependencies...
-pip install pyttsx3 dateparser SpeechRecognition
+echo [3/5] Installing dependencies...
+pip install --upgrade pyttsx3 dateparser SpeechRecognition
 
 echo.
-echo [4/4] Starting Voice Bot...
+echo [4/5] Testing imports...
+python -c "from bot import VoiceAppointmentBot; print('OK')"
+
+echo.
+echo [5/5] Starting Voice Bot...
 echo.
 echo ========================================
 echo BOT IS RUNNING - SPEAK TO IT!
